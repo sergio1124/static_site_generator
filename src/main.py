08afc516cfc5fe73a1ba001df736f9bfaec1a9d7
sergio1124 <mendoza.sergio1124@gmail.com>
2024-12-27 23:11:49 -0600

    Adding text to text node function

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 from textnode import TextNode, TextType
 from htmlnode import HTMLNode
+from inline_markdown import split_nodes_link
 
 def main():
   textNode = TextNode("TESTING TEXT", TextType.BOLD, "URL")
@@ -10,6 +11,21 @@ def main():
   }
   htmlNode = HTMLNode("testTAG", "testValue", [], attributes)
   print(htmlNode)
+
+  node = TextNode(
+    "This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)",
+    TextType.TEXT,
+  )
+  new_nodes = split_nodes_link([node])
+  print(new_nodes)
+  # [
+  #     TextNode("This is text with a link ", TextType.TEXT),
+  #     TextNode("to boot dev", TextType.LINK, "https://www.boot.dev"),
+  #     TextNode(" and ", TextType.TEXT),
+  #     TextNode(
+  #         "to youtube", TextType.LINK, "https://www.youtube.com/@bootdotdev"
+  #     ),
+  # ]
 
 if __name__ == "__main__":
 	main()
