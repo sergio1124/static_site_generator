bb84ccf10bbf10782a03ebd851661d2278a9fafa
sergio1124 <mendoza.sergio1124@gmail.com>
2024-12-29 21:21:48 -0600

    Adding generate pages recursive function

--- src/gencontent.py
+++ src/gencontent.py
@@ -1,6 +1,5 @@
 from markdown_blocks import markdown_to_html_node
-from inline_markdown import extract_title
-
+from pathlib import Path
 import os
 
 def generate_page(from_path, template_path, dest_path):
@@ -19,7 +18,7 @@ def generate_page(from_path, template_path, dest_path):
   # Read contents of template file and store in a variable.
   try:
     template_file = open(template_path, "r")
-    template_contents = template_file.read()
+    template = template_file.read()
   except FileNotFoundError:
     print(f"Error: File not found at path: {template_path}")
   finally:
@@ -34,12 +33,30 @@ def generate_page(from_path, template_path, dest_path):
   title = extract_title(markdown_contents)
 
   # Replacing title in template with the actual title
-  template_with_title = template_contents.replace("{{ Title }}", title)
+  template = template.replace("{{ Title }}", title)
 
   # Replacing content in template with html string
-  template_with_content = template_with_title.replace("{{ Content }}", html_string)
+  template = template.replace("{{ Content }}", html_string)
 
-  # Open the dest_file in write mode
-  with open(dest_path, "w") as file:
-    # Write data to the file
-    file.write(template_with_content)
+  dest_dir_path = os.path.dirname(dest_path)
+  if dest_dir_path != "":
+      os.makedirs(dest_dir_path, exist_ok=True)
+  to_file = open(dest_path, "w")
+  to_file.write(template)
+
+def extract_title(markdown):
+  lines = markdown.split("\n")
+  for line in lines:
+    if line.startswith("# "):
+      return line.lstrip("# ").strip()
+  raise Exception("Invalid markdown, no h1 title")
+
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+  for filename in os.listdir(dir_path_content):
+    from_path = os.path.join(dir_path_content, filename)
+    dest_path = os.path.join(dest_dir_path, filename)
+    if os.path.isfile(from_path):
+      dest_path = Path(dest_path).with_suffix(".html")
+      generate_page(from_path, template_path, dest_path)
+    else:
+      generate_pages_recursive(from_path, template_path, dest_path)
--- src/test_inline_markdown.py
+++ src/test_inline_markdown.py
@@ -6,7 +6,6 @@ from inline_markdown import (
   split_nodes_image,
   split_nodes_link,
   text_to_textnodes,
-  extract_title,
 )
 
 from textnode import TextNode, TextType
@@ -191,44 +190,6 @@ class TestInlineMarkdown(unittest.TestCase):
       ],
       nodes,
     )
-
-  def test_extract_title(self):
-    markdown = """
-# Tolkien Fan Club
-
-**I like Tolkien**. Read my [first post here](/majesty) (sorry the link doesn't work yet)
-
-> All that is gold does not glitter
-
-## Reasons I like Tolkien
-
-* You can spend years studying the legendarium and still not understand its depths
-* It can be enjoyed by children and adults alike
-* Disney *didn't ruin it*
-* It created an entirely new genre of fantasy
-
-## My favorite characters (in order)
-
-1. Gandalf
-2. Bilbo
-3. Sam
-4. Glorfindel
-5. Galadriel
-6. Elrond
-7. Thorin
-8. Sauron
-9. Aragorn
-
-Here's what `elflang` looks like (the perfect coding language):
-
-```
-func main(){
-    fmt.Println("Hello, World!")
-}
-```
-"""
-    title = extract_title(markdown)
-    self.assertEqual(title, "Tolkien Fan Club")
 
 if __name__ == "__main__":
   unittest.main()
